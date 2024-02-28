e868b3344f21833efe29b2283fd6bd8c5fd979bc
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-02-28 10:11:06 +0100

    Add password check and beautify the looks

--- js/script.js
+++ js/script.js
@@ -3,7 +3,35 @@ const pwdConf = document.querySelector("#password-confirmation");
 const btnCreateAcc = document.querySelector("button");
 const divError = document.querySelector("#error-message")
 
-btnCreateAcc.addEventListener("click", () => {
-    divError.textContent = pwd.value !== pwdConf.value ? "Passwords do not match" : "";
-    console.log("hallo");
+let validity = false;
+
+pwd.addEventListener("keyup", () => {
+    if (pwd.checkValidity())
+    {
+        validity = true;
+        btnCreateAcc.disabled = true;
+        if (pwdConf.value === "")
+            divError.textContent = "Plase confirm your password";
+    }
+    else
+    {
+        validity = false;
+        btnCreateAcc.disabled = false;
+    }
+})
+
+pwdConf.addEventListener("keyup", () => {
+    if (validity)
+    {
+        if (pwd.value !== pwdConf.value)
+        {
+            divError.textContent = "Passwords do not match";
+            btnCreateAcc.disabled = true;
+        }
+        else
+        {
+            divError.textContent = "";
+            btnCreateAcc.disabled = false;
+        }
+    }
 });
